8482808fa443897123016b3087117403b245a05c
Dan Petrisko <petrisko@cs.washington.edu>
2021-06-01 04:07:01 -0700

    Adding param headers

--- bp_utils.c
+++ bp_utils.c
@@ -72,4 +72,14 @@ void bp_hprint_uint64(uint64_t val) {
     bp_hprint(nibble);
     val <<= 4;
   }
+
+uint32_t bp_param_get(uint64_t addr) {
+  return *(uint32_t *) addr;
 }
+
+}
+
+uint32_t bp_param_get(uint64_t addr) {
+  return *(volatile uint32_t *) addr;
+}
+
