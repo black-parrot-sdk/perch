f21f7e0ab7bf7b80b0f6a7880c2c6e7d19670e80
Dan Petrisko <petrisko@cs.washington.edu>
2021-07-20 05:01:53 -0700

    Update utils

--- bp_utils.c
+++ bp_utils.c
@@ -14,40 +14,19 @@ uint64_t bp_get_hart() {
     return core_id;
 }
 
-void bp_barrier_end(volatile uint64_t * barrier_address, uint64_t total_num_cores) {
-    uint64_t core_id;
+void bp_barrier_end(volatile uint64_t *barrier_address, uint64_t total_num_cores) {
     uint64_t atomic_inc = 1;
     uint64_t atomic_result;
-    __asm__ volatile("csrr %0, mhartid": "=r"(core_id): :);
+    __asm__ volatile("amoadd.d %0, %2, (%1)": "=r"(atomic_result) 
+                                            : "r"(barrier_address), "r"(atomic_inc)
+                                            :);
     
-    /* if we're not core 0, increment the barrier and then just loop */
-    if (core_id != 0) {
-        __asm__ volatile("amoadd.d %0, %2, (%1)": "=r"(atomic_result) 
-                                                : "r"(barrier_address), "r"(atomic_inc)
-                                                :);
-        while (1) { }
-    }
-    /* 
-     * if we're core 0, increment the barrier as well and then test if the
-     * barrier is equal to the total number of cores
-     */
-    else {
-        uint64_t finish_value = 0;
-        __asm__ volatile("amoadd.d %0, %2, (%1)": "=r"(atomic_result) 
-                                                : "r"(barrier_address), "r"(atomic_inc)
-                                                :);
-        while(*barrier_address < total_num_cores) {
-
-            
-        }
-        bp_finish(0);
-    }
+    while(*barrier_address < total_num_cores);
+    bp_finish(0);
 }
 
 void bp_finish(uint8_t code) {
-  uint64_t core_id;
-
-  __asm__ volatile("csrr %0, mhartid": "=r"(core_id): :);
+  uint64_t core_id = bp_get_hart();
 
   *(FINISH_BASE_ADDR+core_id*8) = code;
 }
@@ -90,7 +69,7 @@ void bp_panic(char *message) {
   while (1);
 }
 
-
 uint32_t bp_param_get(uint64_t addr) {
   return *(volatile uint32_t *) addr;
 }
+
