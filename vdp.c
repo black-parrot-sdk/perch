a94a6b9bc8dcfae0d981344f0bbcbdc7d7e1c4de
Zahra Azad <zazad@bu.edu>
2021-08-13 00:08:57 -0400

    update accelerator lib

--- vdp.c
+++ vdp.c
@@ -18,6 +18,7 @@ uint64_t bp_get_mmio_csr(uint64_t *accel_base_address, uint8_t csr_idx)
 }
 
 
+/////////////////////////////////VDP/////////////////////////////////////////
 void bp_vdp_accelerator_start_cmd(uint64_t *base_cfg_addr){
   bp_set_mmio_csr(base_cfg_addr, ACCEL_VPD_START_CMD, 1);
 }
@@ -61,7 +62,7 @@ void bp_call_vector_dot_product_accelerator(uint8_t type, struct VDP_CSR vdp_csr
   cfg_base_addr = type ? SACCEL_VDP_BASE_ADDR : CACCEL_VDP_BASE_ADDR;
 
   uint64_t *sac_cfg = (uint64_t *) CONFIG_REG_HIO_MASK;
-  bp_set_mmio_csr(sac_cfg, 0, 1);//enable sac mem region csr 
+  bp_set_mmio_csr(sac_cfg, 0, 2); //enable sac mem region csr 
 
   if(type){
     dma_cpy(vdp_csrs.input_a_ptr, SACCEL_VDP_MEM_BASE, vdp_csrs.input_length);
@@ -82,32 +83,6 @@ void bp_call_vector_dot_product_accelerator(uint8_t type, struct VDP_CSR vdp_csr
    }
 }
 
-void bp_call_vector_add_accelerator(uint8_t type, struct VDP_CSR vdp_csrs){
-
-  uint64_t *cfg_base_addr;
-  cfg_base_addr = type ? SACCEL_VADD_BASE_ADDR : CACCEL_VADD_BASE_ADDR;
-  
-  uint64_t *sac_cfg = (uint64_t *) CONFIG_REG_HIO_MASK;
-  bp_set_mmio_csr(sac_cfg, 0, 1);//enable sac mem region csr
-
-  if(type){
-    dma_cpy(vdp_csrs.input_a_ptr, SACCEL_VADD_MEM_BASE, vdp_csrs.input_length);
-    dma_cpy(vdp_csrs.input_b_ptr, SACCEL_VADD_MEM_BASE+vdp_csrs.input_length, vdp_csrs.input_length);
-  }
-
-  uint64_t *src_vec_a, *src_vec_b, *result_vec;
-  src_vec_a =  type ? SACCEL_VADD_MEM_BASE : vdp_csrs.input_a_ptr;
-  src_vec_b =  type ? SACCEL_VADD_MEM_BASE+vdp_csrs.input_length : vdp_csrs.input_b_ptr;
-  result_vec = type ? SACCEL_VADD_MEM_BASE+2*vdp_csrs.input_length : vdp_csrs.resp_ptr;
-
-  bp_vdp_config_accelerator(cfg_base_addr, src_vec_a, src_vec_b, vdp_csrs.input_length, 0, result_vec, 1);
-  bp_vdp_accelerator_start_cmd(cfg_base_addr);
-  bp_vdp_wait_for_completion(cfg_base_addr);
-
-  if(type){
-    dma_cpy(result_vec, vdp_csrs.resp_ptr, 1);
-  }
-}
 
 void bp_hw_dma(uint64_t *cfg_base_dma_addr, uint64_t *src, uint64_t length, uint64_t type)
 {
@@ -125,10 +100,12 @@ void bp_hw_dma(uint64_t *cfg_base_dma_addr, uint64_t *src, uint64_t length, uint
     }
 }
 
+
+/////////////////////////////////ZIPLINE/////////////////////////////////////
 uint64_t bp_call_zipline_accelerator(uint8_t type, struct Zipline_CSR zipline_csrs, uint64_t input_tlv_num)
 {
   uint64_t *sac_cfg = (uint64_t *) CONFIG_REG_HIO_MASK;
-  bp_set_mmio_csr(sac_cfg, 0, 1);//enable sac mem region csr
+  bp_set_mmio_csr(sac_cfg, 0, 2);//enable sac mem region csr
  
   uint64_t *cfg_base_addr;
   uint64_t *cfg_base_dma_addr;
