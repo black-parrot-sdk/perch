28b8bd9eecf859a2a9744f9379f2c4c17b78b7a6
Yuan-Mao <s920721@gmail.com>
2022-02-18 23:44:42 -0700

    Include stdint.h

--- bp_trap.h
+++ bp_trap.h
@@ -1,5 +1,6 @@
 #ifndef BP_TRAP_H
 #define BP_TRAP_H
+#include <stdint.h>
 int register_trap_handler(void (*handler)(uint64_t *, uint64_t, uint64_t),
         unsigned long mcause);
 void bp_trap_handler(uint64_t *regs, uint64_t mcause, uint64_t instr);
