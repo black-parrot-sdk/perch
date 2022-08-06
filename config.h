66a5c498066639a47e588101de83403c7a86ce4c
Dan Petrisko <petrisko@cs.washington.edu>
2022-08-06 16:26:40 -0700

    64b config registers (#11)

--- config.h
+++ config.h
@@ -6,22 +6,22 @@
 
 #define CONFIG_BASE_ADDR 0x200000
 
-#define CONFIG_REG_FREEZE                  ((char *)(CONFIG_BASE_ADDR | 0x00004))
-#define CONFIG_REG_NPC                     ((char *)(CONFIG_BASE_ADDR | 0x00008))
-#define CONFIG_REG_CORE_ID                 ((char *)(CONFIG_BASE_ADDR | 0x0000c))
-#define CONFIG_REG_DID                     ((char *)(CONFIG_BASE_ADDR | 0x00010))
-#define CONFIG_REG_CORD                    ((char *)(CONFIG_BASE_ADDR | 0x00014))
-#define CONFIG_REG_HOST_DID                ((char *)(CONFIG_BASE_ADDR | 0x00018))
-#define CONFIG_REG_HIO_MASK                ((char *)(CONFIG_BASE_ADDR | 0x0001c))
+#define CONFIG_REG_FREEZE                  ((char *)(CONFIG_BASE_ADDR | 0x00008))
+#define CONFIG_REG_NPC                     ((char *)(CONFIG_BASE_ADDR | 0x00010))
+#define CONFIG_REG_CORE_ID                 ((char *)(CONFIG_BASE_ADDR | 0x00018))
+#define CONFIG_REG_DID                     ((char *)(CONFIG_BASE_ADDR | 0x00020))
+#define CONFIG_REG_CORD                    ((char *)(CONFIG_BASE_ADDR | 0x00028))
+#define CONFIG_REG_HOST_DID                ((char *)(CONFIG_BASE_ADDR | 0x00030))
+#define CONFIG_REG_HIO_MASK                ((char *)(CONFIG_BASE_ADDR | 0x00038))
 #define CONFIG_REG_ICACHE_ID               ((char *)(CONFIG_BASE_ADDR | 0x00200))
-#define CONFIG_REG_ICACHE_MODE             ((char *)(CONFIG_BASE_ADDR | 0x00204))
+#define CONFIG_REG_ICACHE_MODE             ((char *)(CONFIG_BASE_ADDR | 0x00208))
 #define CONFIG_REG_DCACHE_ID               ((char *)(CONFIG_BASE_ADDR | 0x00400))
-#define CONFIG_REG_DCACHE_MODE             ((char *)(CONFIG_BASE_ADDR | 0x00404))
+#define CONFIG_REG_DCACHE_MODE             ((char *)(CONFIG_BASE_ADDR | 0x00408))
 #define CONFIG_REG_CCE_ID                  ((char *)(CONFIG_BASE_ADDR | 0x00600))
-#define CONFIG_REG_CCE_MODE                ((char *)(CONFIG_BASE_ADDR | 0x00604))
+#define CONFIG_REG_CCE_MODE                ((char *)(CONFIG_BASE_ADDR | 0x00608))
 #define CONFIG_REG_CCE_UCODE_START         ((char *)(CONFIG_BASE_ADDR | 0x08000))
 #define CONFIG_REG_CCE_UCODE_BASE          ((char *)(CONFIG_REG_CCE_UCODE_START))
-#define CONFIG_REG_CCE_UCODE_END           ((char *)(CONFIG_BASE_ADDR | 0x08FFC))
+#define CONFIG_REG_CCE_UCODE_END           ((char *)(CONFIG_BASE_ADDR | 0x08FF8))
 
 #endif
 
