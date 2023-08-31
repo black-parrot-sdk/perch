8103227816999e48665d0336dc798bafd216e5e9
Dan Petrisko <petrisko@cs.washington.edu>
2023-08-30 17:18:36 -0700

    Adding tensor ops

--- bp_asm.h
+++ bp_asm.h
@@ -31,5 +31,29 @@
   __asm__ __volatile__ (".insn i 0x0f, 0b111, x0, x0, 0x2" : :); \
   })
 
+#define tensor_weight_load0(addr) ({ \
+  __asm__ __volatile__ (".insn i 0x0b, 0b000, %0, %0, 0" : : "r" (addr)); \
+  })
+
+#define tensor_weight_load1(addr) ({ \
+  __asm__ __volatile__ (".insn i 0x0b, 0b001, %0, %0, 0" : : "r" (addr)); \
+  })
+
+#define tensor_accum_load0(addr) ({ \
+  __asm__ __volatile__ (".insn i 0x0b, 0b010, %0, %0, 0" : : "r" (addr)); \
+  })
+
+#define tensor_accum_load1(addr) ({ \
+  __asm__ __volatile__ (".insn i 0x0b, 0b011, %0, %0, 0" : : "r" (addr)); \
+  })
+
+#define tensor_csr_ld(csr) ({ unsigned long __tmp; \
+  __asm__ __volatile__ (".insn i 0x0b, 0b100, %0, x0, %1" : "=r"(__tmp) : "I"(csr)); \
+  __tmp; })
+
+#define tensor_csr_st(csr, val) ({ \
+  __asm__ __volatile__ (".insn i 0x0b, 0b100, x0, %0, " #csr "" : : "rK"(val)); \
+  })
+
 #endif
 
